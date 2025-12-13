89aeac677249a892c82a05260cc4bc16e3ffb739
miske11 <lukamisic2001@gmail.com>
2025-12-13 23:06:59 +0100

    player sprite that can rotate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -8,6 +9,11 @@ def main():
     print("Screen height:", SCREEN_HEIGHT)
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True:
         log_state()
@@ -16,7 +22,10 @@ def main():
                 return
 
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
-
+        time_passed = clock.tick(60)
+        dt = time_passed / 1000
 if __name__ == "__main__":
     main()
